327881a33d9b0edcea9741057aeed2193b0c1d8c
hemanthio <121523327+hemanthio@users.noreply.github.com>
2025-01-31 18:24:05 +0530

    hero section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,42 @@
-import Link from 'next/link';
+'use client'
+
+import { Check } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 export default function HomePage() {
+  const router = useRouter();
+
+  const handleButtonClick = () => {
+    router.push('/docs');
+  };
+
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+    <main className="flex flex-1 flex-col gap-3 justify-center items-center text-center">
+
+      <h1 className='text-5xl font-semibold  tracking-[-0.7px] leading-[140%] '>The <span className='p-1  bg-purple-500  '>Ultimate-NextJS </span> <br /> Components Library </h1>
+
+      <p className='font-light opacity-85'>build your website <span className='font-medium opacity-100'>user-interface </span>  in minutes like <span className='font-medium opacity-100'> thunder</span> ⚡</p>
+
+      
+      <ul className="list-none flex flex-col gap-1 mt-5 font-medium  items-start">
+        <li className="flex items-center gap-2">
+          <Check color="#AD46FF" />
+          Collection of sections, pages and components 
+        </li>
+        <li className="flex items-center gap-2">
+          <Check color="#AD46FF" />
+          Easy integration and quick development
+        </li>
+        <li className="flex items-center gap-2">
+          <Check color="#AD46FF" />
+          Responsive across all devices
+        </li>
+      </ul>
+      <button 
+        onClick={handleButtonClick}
+        className='font-medium mt-6 cursor-pointer rounded-sm text-base py-3 px-8 text-white bg-orange-400'>
+        Explore documentation-<span className='text-sm opacity-75'>its free</span>
+      </button>
     </main>
   );
 }
--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,39 +1,23 @@
-'use client';
 
-import React, { useState } from 'react';
-import { motion } from 'motion/react';
+
 import { MoveRight } from 'lucide-react';
 
+
+
 const Button = () => {
-  const [isHovered, setIsHovered] = useState(false);
+
 
   return (
-    <div
-      className='flex gap-2 cursor-pointer text-white items-center py-2 pr-2 pl-4 bg-[#F15024] rounded-full'
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
+    <div 
+    
+      className='flex gap-2 cursor-pointer text-white items-center py-2 pr-2 pl-4 bg-[#F15024] rounded-full' >
       <h1 className='text-base font-medium'>Register Now</h1>
-      <div className='p-2 rounded-full bg-white relative overflow-hidden'>
-        {/* First Arrow */}
-        <motion.div
-          initial={{ x: 0 }}
-          animate={{ x: isHovered ? '100%' : 0 }}
-          transition={{ type: 'spring', stiffness: 300, damping: 20 }}
-          style={{ position: 'absolute', left: 0 }}
-        >
+      <div className='p-2 rounded-full bg-white '>
+      
+       <div>
           <MoveRight color='#F15024' />
-        </motion.div>
-
-        {/* Second Arrow (hidden initially) */}
-        <motion.div
-          initial={{ x: '-100%' }}
-          animate={{ x: isHovered ? 0 : '-100%' }}
-          transition={{ type: 'spring', stiffness: 300, damping: 20 }}
-          style={{ position: 'absolute', left: 0 }}
-        >
-          <MoveRight color='#F15024' />
-        </motion.div>
+          </div>
+      
       </div>
     </div>
   );
