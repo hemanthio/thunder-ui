67db9e913ad8e4e6efad4ae6e2572e426f5d61c7
hemanthio <121523327+hemanthio@users.noreply.github.com>
2025-02-01 01:13:43 +0530

    updated the code and added the animation to the arrowbutton

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -13,12 +13,12 @@ export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col gap-3 justify-center items-center text-center">
 
-      <h1 className='text-5xl font-semibold  tracking-[-0.7px] leading-[140%] '>The <span className='p-1  bg-purple-500  '>Ultimate-NextJS </span> <br /> Components Library </h1>
+      <h1 className='text-5xl max-sm:text-3xl font-semibold  tracking-[-0.7px] leading-[140%] '>The <span className='p-1  bg-purple-500  '>Ultimate-NextJS </span> <br /> Components Library </h1>
 
       <p className='font-light opacity-85'>build your website <span className='font-medium opacity-100'>user-interface </span>  in minutes like <span className='font-medium opacity-100'> thunder</span> ⚡</p>
 
       
-      <ul className="list-none flex flex-col gap-1 mt-5 font-medium  items-start">
+      <ul className="list-none flex flex-col gap-1 max-sm:gap-2 mt-5 font-medium  items-start">
         <li className="flex items-center gap-2">
           <Check color="#AD46FF" />
           Collection of sections, pages and components 
--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,23 +1,41 @@
 
-
+'use client'
 import { MoveRight } from 'lucide-react';
-
-
+import { motion, AnimatePresence } from "motion/react";
+import { useState } from 'react';
 
 const Button = () => {
-
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div 
-    
-      className='flex gap-2 cursor-pointer text-white items-center py-2 pr-2 pl-4 bg-[#F15024] rounded-full' >
+    <div
+      className='flex gap-2 cursor-pointer text-white items-center py-2 pr-2 pl-4 bg-[#F15024] rounded-full'
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <h1 className='text-base font-medium'>Register Now</h1>
-      <div className='p-2 rounded-full bg-white '>
-      
-       <div>
-          <MoveRight color='#F15024' />
-          </div>
-      
+      <div className='p-2 rounded-full flex bg-white overflow-hidden relative '>
+        <AnimatePresence mode="popLayout">
+          {!isHovered ? (
+            <motion.div
+              key="first"
+              initial={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 31 }}
+              transition={{ duration: 0.3 }}
+            >
+              <MoveRight color='#F15024' />
+            </motion.div>
+          ) : (
+            <motion.div
+              key="second"
+              initial={{ opacity: 0, x: -31 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              <MoveRight color='#F15024' />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
